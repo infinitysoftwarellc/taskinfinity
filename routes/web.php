fdd1514eaeacc59eb4498fd8632a5b1fa8612d97
guilhermerodrigues2121 <guilhermeinfinity0@gmail.com>
2025-09-23 02:25:21 +0000

    configurando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Task\TaskController;
 
-Route::view('/', 'welcome');
+Route::middleware(['auth'])->prefix('webapp')->name('webapp')->group(function () {
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    Route::get('/task', [TaskController::class, 'index'])
+        ->middleware(['verified'])
+        ->name('task.index');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('/profile', 'profile')
+        ->name('profile');
+
+});
 
 require __DIR__.'/auth.php';
