da1213ce6f9d705c7d9246d559b4f56bbe9b1e1e
Guilherme Rodrigues <guilhermeinfinity0@gmail.com>
2025-09-25 11:39:32 -0300

    feat: add server-driven pomodoro timer

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -75,5 +76,15 @@ class User extends Authenticatable
     public function tasks(): HasMany
     {
         return $this->hasMany(Task::class, 'user_id');
+    }
+
+    public function pomodoroSetting(): HasOne
+    {
+        return $this->hasOne(PomodoroSetting::class);
+    }
+
+    public function pomodoroSessions(): HasMany
+    {
+        return $this->hasMany(PomodoroSession::class);
     }
 }
