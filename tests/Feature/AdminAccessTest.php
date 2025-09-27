5d1cc6c070465d5b92382b4604d237f5a6069148
Guilherme Rodrigues <guilhermeinfinity0@gmail.com>
2025-09-27 12:30:21 -0300

    Redirect non-admin users away from admin routes

---
+++ tests/Feature/AdminAccessTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+
+test('non-admin users are redirected from admin analytics', function () {
+    $user = User::factory()->create(['role' => User::ROLE_USER]);
+
+    $this->actingAs($user);
+
+    $response = $this->get(route('admin.analytics'));
+
+    $response->assertRedirect(route('tasks.index'));
+});
+
+test('non-admin users are redirected from the admin dashboard routes', function () {
+    $user = User::factory()->create(['role' => User::ROLE_USER]);
+
+    $this->actingAs($user);
+
+    $response = $this->get(route('dashboard.index'));
+
+    $response->assertRedirect(route('tasks.index'));
+});
