cfb740b9445bce978931f4492ee04e5a63ee9857
Guilherme Rodrigues <guilhermeinfinity0@gmail.com>
2025-09-30 14:59:50 -0300

    Expand description editor toolbar options

--- resources/js/app.js
+++ resources/js/app.js
@@ -401,20 +401,38 @@ function setupDetailsKeyboard() {
 
 const QUILL_TOOLBAR_MARKUP = `
   <span class="ql-formats">
+    <select class="ql-header">
+      <option selected></option>
+      <option value="2"></option>
+      <option value="3"></option>
+    </select>
+    <select class="ql-font"></select>
+  </span>
+  <span class="ql-formats">
     <button class="ql-bold"></button>
     <button class="ql-italic"></button>
     <button class="ql-underline"></button>
     <button class="ql-strike"></button>
+    <button class="ql-code"></button>
   </span>
   <span class="ql-formats">
+    <button class="ql-blockquote"></button>
+    <button class="ql-code-block"></button>
     <button class="ql-list" value="ordered"></button>
     <button class="ql-list" value="bullet"></button>
     <button class="ql-list" value="check"></button>
-    <button class="ql-blockquote"></button>
+    <button class="ql-indent" value="-1"></button>
+    <button class="ql-indent" value="+1"></button>
+  </span>
+  <span class="ql-formats">
+    <select class="ql-align"></select>
+    <select class="ql-color"></select>
+    <select class="ql-background"></select>
   </span>
   <span class="ql-formats">
     <button class="ql-link"></button>
     <button class="ql-image"></button>
+    <button class="ql-clean"></button>
   </span>
 `;
 
