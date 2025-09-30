b1a1d962f04a8555926e10e953a359e77e95cfe2
guilhermerodrigues2121 <guilhermeinfinity0@gmail.com>
2025-09-30 03:26:11 +0000

    add

--- resources/views/app/tasks/index.blade.php
+++ resources/views/app/tasks/index.blade.php
@@ -1,4 +1,4 @@
 {{-- resources/views/app/tasks/index.blade.php --}}
 <x-app-layout>
-            <livewire:tasks.board :list-id="$listId ?? null" :shortcut="$shortcut ?? null" />
+    <livewire:tasks.board :list-id="$listId ?? null" :shortcut="$shortcut ?? null" />
 </x-app-layout>
